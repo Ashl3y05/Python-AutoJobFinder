a3873684219a77e8b8e083fa35a0861ac3f87e8e
Ashley <panelo.ashley@gmail.com>
2025-07-02 22:35:47 +0800

    Will now login using env variables

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
+from time import time, sleep
 
-USERNAME = ""
-PASSWORD = ""
+USERNAME = os.environ["LINKEDIN_USER"]
+PASSWORD = os.environ["LINKEDIN_PASS"]
+
+
+def sign_in():
+    enter = driver.find_element(By.LINK_TEXT, "Sign in")
+    enter.click()
+
 edge_option = webdriver.EdgeOptions()
 edge_option.add_experimental_option("detach", True)
 
 driver = webdriver.Edge(options=edge_option)
 driver.get("https://www.linkedin.com/jobs/")
 
-sign_in = driver.find_element(By.LINK_TEXT, "Sign in")
-sign_in.click()
+sign_in()
+
+sleep(3)
+
+user_text = driver.find_element(By.ID, "username")
+pass_text = driver.find_element(By.ID, "password")
+
+user_text.send_keys(USERNAME)
+pass_text.send_keys(PASSWORD)
+
+submit = driver.find_element(By.CSS_SELECTOR, ".login__form_action_container button")
+submit.click()
+
+
