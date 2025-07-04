391dd713422d55f86558f5b7e00a1717d0f6488e
Ashley <panelo.ashley@gmail.com>
2025-07-04 21:07:18 +0800

    Will now loop through the Python dev jobs - Still have problem saving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions
+
 import os
 from time import time, sleep
 
@@ -33,7 +35,20 @@ submit.click()
 
 sleep(3)
 
-search_box = driver.find_element(By.XPATH, value='//*[@id="jobs-search-box-keyword-id-ember213"]')
+search_box = driver.find_element(By.CLASS_NAME, 'jobs-search-box__text-input--rounded')
 search_box.send_keys("Python Developer", Keys.ENTER)
+
+sleep(2)
+
+jobs = driver.find_elements(By.CLASS_NAME, 'eXpUlPeraWOMWeQsOLgQAYzhjWpNMjOpAHpxA')
+save_job = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div[6]/div/button')
+
+for item in jobs:
+    item.click()
+    sleep(1)
+    try:
+        save_job.click()
+    except selenium.common.StaleElementReferenceException:
+        print("Error")
 
 
